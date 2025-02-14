bb53cced84aaa18dbdae781d23d3fb57d5647e99
Marquittos01 <marcosantoniomoralesserrano@gmail.com>
2025-02-14 13:23:06 +0100

    Commit interfaz y extends

--- src/main/java/com/tarea/controllers/ControladorTarea.java
+++ src/main/java/com/tarea/controllers/ControladorTarea.java
@@ -2,6 +2,9 @@ package com.tarea.controllers;
 
 import com.tarea.entities.Tarea;
 import com.tarea.services.ServicioTarea;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 public class ControladorTarea {
 
@@ -11,4 +14,11 @@ public class ControladorTarea {
     public ControladorTarea(){
         this.tarea = this.serviciotarea.getTarea();
     }
+
+    @GetMapping("/tareas")
+    public List<Tarea> getAllTareas(){
+        return this.serviciotarea.getAllTareas();
+    }
+
+
 }
--- src/main/java/com/tarea/services/ServicioTarea.java
+++ src/main/java/com/tarea/services/ServicioTarea.java
@@ -1,15 +1,21 @@
 package com.tarea.services;
 
 import com.tarea.entities.Tarea;
+import com.tarea.repositories.RepositoryTarea;
 
-import java.time.LocalDate;
+import java.util.List;
 
 public class ServicioTarea {
 
+    private RepositoryTarea repository;
     Tarea tarea;
 
-    public ServicioTarea() {
-        this.tarea = new Tarea(1L,"Tarea1","Tarea",true, LocalDate.now());
+    public ServicioTarea(RepositoryTarea repository) {
+        this.repository = repository;
+    }
+
+    public List<Tarea> getAllTareas() {
+        return this.repository.findAll();
     }
 
     public Tarea getTarea() {
