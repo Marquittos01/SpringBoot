727a5a3c3999a1454f49820e40a4d04cd2bf6461
osopardo895 <alexcesarfer1234@gmail.com>
2025-02-14 13:57:45 +0100

    amn

--- src/main/java/com/tarea/controllers/ControladorTarea.java
+++ src/main/java/com/tarea/controllers/ControladorTarea.java
@@ -3,9 +3,11 @@ package com.tarea.controllers;
 import com.tarea.entities.Tarea;
 import com.tarea.services.ServicioTarea;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class ControladorTarea {
 
     private Tarea tarea;
