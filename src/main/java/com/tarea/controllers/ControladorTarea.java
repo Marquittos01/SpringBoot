8189eb15c60bb58047c640ef0b55213cec0c5f17
Marquittos01 <marcosantoniomoralesserrano@gmail.com>
2025-02-17 10:39:58 +0100

    Commit crud completo

--- src/main/java/com/tarea/controllers/ControladorTarea.java
+++ src/main/java/com/tarea/controllers/ControladorTarea.java
@@ -2,8 +2,7 @@ package com.tarea.controllers;
 
 import com.tarea.entities.Tarea;
 import com.tarea.services.ServicioTarea;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +15,30 @@ public class ControladorTarea {
         this.serviciotarea = serviciotarea;
     }
 
-    @GetMapping("/tareas")
-    public List<Tarea> tareas(){
+
+    @GetMapping("/tarea")
+    public List<Tarea> getAllTareas(){
         return this.serviciotarea.getAllTareas();
+    }
+
+    @GetMapping("/tarea/{id}")
+    public Tarea getTarea(@PathVariable Long id){
+        return this.serviciotarea.getTarea(id);
+    }
+
+    @PostMapping("/tarea")
+    public Tarea createTarea(@RequestBody Tarea tarea) {
+        return this.serviciotarea.createTarea(tarea);
+    }
+
+    @PutMapping("/tarea")
+    public Tarea updateTarea(@RequestParam Long id, @RequestBody Tarea tareaDetalles) {
+        return this.serviciotarea.updateTarea(id, tareaDetalles);
+    }
+
+    @DeleteMapping("/tarea")
+    public void deleteTarea(@RequestParam Long id) {
+        this.serviciotarea.deleteTarea(id);
     }
 
 }
--- src/main/java/com/tarea/services/ServicioTarea.java
+++ src/main/java/com/tarea/services/ServicioTarea.java
@@ -5,6 +5,7 @@ import com.tarea.repositories.RepositoryTarea;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServicioTarea {
@@ -20,7 +21,33 @@ public class ServicioTarea {
     }
 
     public Tarea getTarea(Long id) {
-        return this.repositoryTarea.findById(id).get();
+        return this.repositoryTarea.findById(id).orElse(null);
     }
+
+    public Tarea createTarea(Tarea tarea) {
+        return this.repositoryTarea.save(tarea);
+    }
+
+    public Tarea updateTarea(Long id, Tarea tareaDetalles) {
+        Optional<Tarea> tareaOptional = this.repositoryTarea.findById(id);
+
+        if (tareaOptional.isPresent()) {
+            Tarea tarea = tareaOptional.get();
+            tarea.setNombre(tareaDetalles.getNombre());
+            tarea.setDescripcion(tareaDetalles.getDescripcion());
+            tarea.setEstado(tareaDetalles.getEstado());
+            tarea.setFecha(tareaDetalles.getFecha());
+            return this.repositoryTarea.save(tarea);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteTarea(Long id) {
+        if (this.repositoryTarea.existsById(id)) {
+            this.repositoryTarea.deleteById(id);
+        }
+    }
+
 }
 
